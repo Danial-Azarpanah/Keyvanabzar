4141f0754d7000bff88399c948ccd32cd2df210a
aliteymouri <aliteymr@gmail.com>
2023-05-23 13:32:12 +0330

    Added product model and fieldsets

--- Dewalt/urls.py
+++ Dewalt/urls.py
@@ -7,5 +7,6 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
-    path('', include("accounts.urls"))
+    path('', include("accounts.urls")),
+    path('', include("product.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ product/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'price')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'price')
+    prepopulated_fields = {'slug': ('title',)}
+
+    fieldsets = [
+        ("نام و دسته بندی محصول",
+         {
+             'fields': ('title',),
+         }),
+        ('مشخصات محصول',
+         {
+             'fields': ('price', 'discount', 'weight')
+         }),
+        ('توضیحات و تصویر محصول',
+         {
+             'classes': ('collapse', 'open'),
+             'fields': ['description', 'image', 'power_battery', 'maximum_torque',
+                        'speed_range', 'hammer_mode', 'spare_battery', 'box', 'slug']
+
+         }),
+    ]
