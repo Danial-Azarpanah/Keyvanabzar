7c7e6466ac9edbcba355de6372a6f92dcf2e4a37
aliteymouri <aliteymr@gmail.com>
2023-05-25 16:13:22 +0330

    show favorite list of user

--- product/urls.py
+++ product/urls.py
@@ -5,4 +5,5 @@ app_name = 'product'
 urlpatterns = [
     path('product-list/', views.ProductListView.as_view(), name='product-list'),
     path('add-favorite/<int:pk>', views.AddFavoriteView.as_view(), name='add-favorite'),
+    path('favorite-list/', views.FavoriteListView.as_view(), name='favorite-list'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect
 from django.views.generic import *
+from django.shortcuts import *
+from accounts.mixins import *
 from product.models import *
 
 
@@ -12,10 +13,18 @@ class ProductListView(ListView):
 
 
 class AddFavoriteView(View):
-    def get(self, request, pk):
+    def get(self, req, pk):
         try:
-            fav = Favorite.objects.get(product_id=pk, user_id=request.user.id)
+            fav = Favorite.objects.get(product_id=pk, user_id=req.user.id)
             fav.delete()
         except:
-            Favorite.objects.create(product_id=pk, user_id=request.user.id)
-        return redirect('home:home')
+            Favorite.objects.create(product_id=pk, user_id=req.user.id)
+        return redirect('product:favorite-list')
+
+
+class FavoriteListView(RequiredLoginMixin, View):
+    template_name = "product/favorite-list.html"
+
+    def get(self, req, **kwargs):
+        favorites = Product.objects.filter(favorites__user=req.user)
+        return render(req, self.template_name, {"favorites": favorites})
