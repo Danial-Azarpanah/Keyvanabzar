606016e79fd7a6f02c6301e7c166d420330814cb
Danial-Azarpanah <danial.azarpanah.dev@gmail.com>
2023-05-28 14:58:40 +0330

    Displayed product details and specs in product-detail page

--- product/views.py
+++ product/views.py
@@ -12,9 +12,22 @@ class ProductListView(ListView):
     paginate_by = 10
 
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'product/product-detail.html'
+class ProductDetailView(View):
+
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        spec_list = product.specifications
+        fields_with_values = []
+        for field in spec_list._meta.fields[2:]:
+            value = getattr(spec_list, field.name)
+            if value:
+                if value == True:
+                    fields_with_values.append((field.verbose_name, "دارد"))
+                else:
+                    fields_with_values.append((field.verbose_name, value))
+        print(fields_with_values)
+        return render(request, "product/product-detail.html", {"product": product,
+                                                               "specs": fields_with_values})
 
 
 class AddFavoriteView(View):
