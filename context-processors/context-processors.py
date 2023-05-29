6b9f47ea128e5d54cc8d32cd97fb1721d2e75012
aliteymouri <aliteymr@gmail.com>
2023-05-29 19:50:43 +0330

    apply some changes in header

--- context-processors/context-processors.py
+++ context-processors/context-processors.py
@@ -1,6 +1,10 @@
 from product.models import Category
-
+from payment.cart import Cart
 
 def category_list(req):
     categories = Category.objects.all()
     return {'categories': categories}
+
+
+def cart_info(req):
+    return {'cart': Cart(req)}
