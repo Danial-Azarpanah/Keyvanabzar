31fb622c57ef1d9faafe3795d6dd5ad79ada4ff0
Danial-Azarpanah <danial.azarpanah.dev@gmail.com>
2023-07-12 18:06:32 +0330

    Applied server config

--- Dewalt/settings.py
+++ Dewalt/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["www.dewalt-land.com", "dewalt-land.com"]
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.dewalt-land.com", "dewalt-land.com"]
 CSRF_TRUSTED_ORIGINS = ['https://*.dewalt-land.com']
 
 # Application definition
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'Dewalt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': "dewaltla_dewalt-land",
-#         "USER": "dewaltla_admin",
-#         "PASSWORD": "daniel8203",
-#         "HOST": "127.0.0.1",
-#         "PORT": "3306"
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "dewaltla_dewalt-land",
+        "USER": "dewaltla_admin",
+        "PASSWORD": "daniel8203",
+        "HOST": "127.0.0.1",
+        "PORT": "3306"
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -135,12 +135,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     path.join(BASE_DIR, "assets")
 ]
-# STATIC_ROOT = "/home/dewaltla/public_html/static"
-STATIC_ROOT = path.join(BASE_DIR, "static")
+STATIC_ROOT = "/home/dewaltla/public_html/static"
+# STATIC_ROOT = path.join(BASE_DIR, "static")
 
 MEDIA_URL = 'media/'
-# MEDIA_ROOT = "/home/dewaltla/public_html/media"
-MEDIA_ROOT = path.join(BASE_DIR, "media")
+MEDIA_ROOT = "/home/dewaltla/public_html/media"
+# MEDIA_ROOT = path.join(BASE_DIR, "media")
 TEMPLATE_CONTEXT_PROCESSORS = "django.contrib.messages.context_processors.messages"
 
 TEMPLATE_CONTEXT_PROCESSORS = "django.contrib.messages.context_processors.messages"
