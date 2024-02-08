840d95dacc418aea9f4797173c113ab4ca5129b7
Danial-Azarpanah <danial.azarpanah.dev@gmail.com>
2024-02-08 21:23:27 +0330

    Changed domain to keybanabzar.com

--- Dewalt/settings.py
+++ Dewalt/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["www.dewalt-land.com", "dewalt-land.com"]
+ALLOWED_HOSTS = ["www.keyvanabzar.com", "keyvanabzar.com"]
 CSRF_TRUSTED_ORIGINS = ['https://*.dewalt-land.com']
 
 
